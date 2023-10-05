e252eb261c2f97b7115555c1935efdcab125e21b
TheIns07 <arturoinscreup2401@hotmail.com>
2023-10-05 00:04:36 -0700

    Add project files.

---
+++ TrainingC#/CollectionsExample.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingC_
+{
+    public class CollectionsExample
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car
+            {
+                Model = "Honda",
+                Make = "2000"
+            };
+
+            Book book = new Book
+            {
+                Author = "Author",
+                ISBN = "ISBN",
+                Name = "Name"
+            };
+
+            Car car1 = new Car
+            {
+                Model = "Honda",
+                Make = "2000"
+            };
+
+            Book book1 = new Book
+            {
+                Author = "Jose",
+                ISBN = "ISBN",
+                Name = "Name"
+            };
+            Dictionary<string, Book> myDictionary = new Dictionary<string, Book>();
+            myDictionary.Add(book.Author, book);
+            myDictionary.Add(book1.Author, book1);
+
+            List<Car> listCars = new List<Car> { 
+                new Car{ Make = "12321", Model = "BWM", Year=2024},
+                new Car {Make = "2TGE4", Model = "FORD", Year=2021},
+                new Car{ Make = "12321", Model = "BWM", Year = 2022}
+
+            };
+
+            //LINQ Query
+            var orderedCars = from myCar in listCars 
+                              orderby myCar.Year ascending
+                              select myCar;
+            //LINQ Method
+            var bmws = listCars.Where(p => p.Model == "BWM" && p.Make == "12321").OrderByDescending(p=> p.Year);
+
+            //Boolean Comprobation
+            Console.WriteLine(listCars.TrueForAll(p => p.Year > 2020));
+
+            foreach (var carrito in listCars)
+            {
+                Console.WriteLine("{0} {1}", carrito.Year, carrito.Model, carrito.Make);
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+        class Car
+        {
+            public string Model { get; set; }
+            public string Make { get; set; }
+            public int Year { get; set; }
+
+        }
+
+        class Book
+        {
+            public string Name { get; set; }
+            public string Author { get; set; }
+            public string ISBN { get; set; }
+        }
+    }
+}
---
+++ TrainingC#/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using TestProject;
+
+namespace Program
+{
+    class Program
+    {
+        // static void Main(string[] args)
+
+        void method()
+        {
+            Scrape myScrape = new();
+            Console.WriteLine(myScrape.ScrapeWebPage("https://example-files.online-convert.com/document/txt/example.txt"));
+            Console.ReadLine();
+        }
+ 
+    }
+}
